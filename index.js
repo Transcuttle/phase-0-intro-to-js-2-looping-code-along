136f650d1c8e745016c84834fd022ac3b72ea329
George <georgefbrockman@gmail.com>
2023-05-25 18:37:42 -0500

    Finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    const cards = [];
+
+    for (let i = 0; i < array.length; i++){
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(start){
+    while(start >= 0){
+        console.log(start);
+        start--;
+    }
+}
